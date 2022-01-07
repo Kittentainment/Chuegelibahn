65aabae5cee949f7ddbbb4f59631a633d7169c43
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2022-01-07 22:06:47 +0100

    Max number of elements++ for curve tiles

--- TrackPrinterVR/Assets/Scripts/TrackPrinting/TrackPrefabManager.cs
+++ TrackPrinterVR/Assets/Scripts/TrackPrinting/TrackPrefabManager.cs
@@ -125,7 +125,7 @@ public class TrackPrefabManager : MonoBehaviour
         return type switch
         {
             TrackType.Straight => 50,
-            TrackType.Left => 38,
+            TrackType.Left => 39,
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
         };
     }
