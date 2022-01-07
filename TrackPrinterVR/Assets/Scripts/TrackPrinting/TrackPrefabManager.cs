5e74c66994a1502b565fe6f8655801a3d24cd22d
Flavia Brogle <flavia.brogle@students.fhnw.ch>
2022-01-07 22:17:22 +0100

    Add one piece to curve

--- TrackPrinterVR/Assets/Scripts/TrackPrinting/TrackPrefabManager.cs
+++ TrackPrinterVR/Assets/Scripts/TrackPrinting/TrackPrefabManager.cs
@@ -125,7 +125,7 @@ public class TrackPrefabManager : MonoBehaviour
         return type switch
         {
             TrackType.Straight => 50,
-            TrackType.Left => 39,
+            TrackType.Left => 40,
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
         };
     }
